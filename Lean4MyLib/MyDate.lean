59a85eb812a4c2d0b2d694c47cae597e242f8bb4
shinamon <76248816+shinamon610@users.noreply.github.com>
2025-04-27 23:46:15 +0900

    expand zdt

--- Lean4MyLib/MyDate.lean
+++ Lean4MyLib/MyDate.lean
@@ -19,13 +19,8 @@ def JST : TimeZone :=
 def now:=(ZonedDateTime.nowAt "Japan")
 def is_same_date (a b:ZonedDateTime):Bool:= a.year==b.year && a.month==b.month && a.day== b.day
 def same_date (a b:ZonedDateTime):Prop:=is_same_date a b = true
-def zdt (year : Year.Offset) (month : Month.Ordinal) (day : Day.Ordinal) := ZonedDateTime.ofPlainDateWithZone (PlainDate.ofYearMonthDayClip year month day) JST
 
-#eval PlainDate.ofYearMonthDayClip 2020 15 33
-#eval PlainDate.ofYearMonthDay? 2020 15 33 -- Optionなんだけど、絶対にsomeが帰ってくるから意味ない。
-#eval ZonedDateTime.ofPlainDateWithZone (PlainDate.ofYearMonthDayClip 2020 15 33) JST |> fun x => x.toTimestamp.val.toMilliseconds
-#eval ZonedDateTime.of (PlainDateTime.mk (.ofYearMonthDayClip 2020 15 33) (.ofHourMinuteSeconds 100 100 10)) "Japan"
-
-#eval (ZonedDateTime.nowAt "Japan") >>= fun x=> (ZonedDateTime.nowAt "Japan") >>= fun y => return decide (x == y)
-
-#eval now >>= fun x=> return is_same_date x (zdt 2025 3 13 )
+def zdt (year : Year.Offset) (month : Month.Ordinal) (day : Day.Ordinal)
+        (hour : Hour.Ordinal:=0) (minute : Minute.Ordinal:=0) : ZonedDateTime :=
+  let pdt := PlainDateTime.mk (PlainDate.ofYearMonthDayClip year month day) (PlainTime.ofHourMinuteSeconds hour minute 0)
+  ZonedDateTime.ofPlainDateTimeWithZone pdt JST
